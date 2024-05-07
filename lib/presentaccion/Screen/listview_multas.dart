57299b94453ded4088c84bce3b1a907b8242042a
josejesus3 <gutierresrayas88@gmail.com>
2024-05-07 13:42:30 -0600

    sin anime_do

--- lib/presentaccion/Screen/listview_multas.dart
+++ lib/presentaccion/Screen/listview_multas.dart
@@ -1,4 +1,3 @@
-import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:multas/domain/conexion/connection_mysql.dart';
