d3f01cf283ed1e9ac7930bb2babed7551641ebe9
josejesus3 <gutierresrayas88@gmail.com>
2024-06-05 14:08:41 -0600

    busqueda en formulario

--- lib/presentaccion/Screen/home_screen.dart
+++ lib/presentaccion/Screen/home_screen.dart
@@ -1,21 +1,78 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:multas/domain/conexion/connection_mysql.dart';
+import 'package:multas/domain/entiti/list_multas.dart';
 import 'package:multas/presentaccion/Widget/app_bar_buscador.dart';
 import 'package:multas/presentaccion/Widget/custom_botton_navigator.dart';
 import 'package:multas/presentaccion/Widget/formulario.dart';
-
 import 'package:multas/presentaccion/provider/read_provider.dart';
 
-class HomeScreen extends ConsumerWidget {
+class HomeScreen extends ConsumerStatefulWidget {
   static const String name = 'HomeScreen';
-  const HomeScreen({super.key});
+
+  const HomeScreen({Key? key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  HomeScreenState createState() => HomeScreenState();
+}
+
+class HomeScreenState extends ConsumerState<HomeScreen> {
+  TextEditingController placaController = TextEditingController();
+  TextEditingController cantidadController = TextEditingController();
+  TextEditingController fechaController = TextEditingController();
+  TextEditingController folioController = TextEditingController();
+  TextEditingController folioPagoController = TextEditingController();
+  TextEditingController cantidadFolioController = TextEditingController();
+  TextEditingController insfraccionController = TextEditingController();
+  TextEditingController fechasPagoController = TextEditingController();
+  TextEditingController foraneasController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
     final int index = ref.watch(currentIndex);
     final textStyle = Theme.of(context).textTheme;
-
     TextEditingController searchController = TextEditingController();
+
+    void searchMultas(String placa, String fecha) async {
+      List<ListMultas> searchedMultas =
+          await ConnectionMysql().selectQueryBusqueda(placa, fecha);
+      if (searchedMultas.isNotEmpty) {
+        // Mostrar los datos encontrados en los TextField
+        setState(() {
+          placaController.text = searchedMultas[0].placa!;
+          cantidadController.text = searchedMultas[0].cantidadPago.toString();
+          fechaController.text = searchedMultas[0].fecha!;
+          folioController.text = searchedMultas[0].folio.toString();
+          folioPagoController.text = searchedMultas[0].folioPago!;
+          cantidadFolioController.text =
+              searchedMultas[0].cantidadPago.toString();
+          // Ajusta los demás campos según sea necesario
+        });
+      } else {
+        // Si no se encontraron datos, puedes mostrar un mensaje o limpiar los campos
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(
+            duration: Duration(milliseconds: 500),
+            backgroundColor: Colors.black12,
+            content: ListTile(
+              title: Text('No se encontraron multas'),
+            ),
+          ),
+        );
+        // Limpiar los TextField
+        setState(() {
+          placaController.clear();
+          cantidadController.clear();
+          fechaController.clear();
+          folioController.clear();
+          folioPagoController.clear();
+          cantidadFolioController.clear();
+          // Limpia los demás campos según sea necesario
+        });
+      }
+    }
+
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.white,
@@ -26,7 +83,7 @@ class HomeScreen extends ConsumerWidget {
             ),
             Text(
               'Multas',
-              style: textStyle.titleLarge,
+              style: textStyle!.titleLarge,
             ),
             const SizedBox(
               width: 20,
@@ -42,16 +99,34 @@ class HomeScreen extends ConsumerWidget {
               'Status: A',
               style: textStyle.bodyLarge,
             ),
-            Spacer(),
-            getAppBarSearching(searchController, () {}, () {}),
+            const Spacer(),
+            getAppBarSearching(searchController, () {
+              setState(() {
+                searchMultas(searchController.text, searchController.text);
+              });
+            }, () {}),
           ],
         ),
         bottom: const PreferredSize(
-            preferredSize: Size.fromHeight(10), child: Divider()),
+          preferredSize: Size.fromHeight(10),
+          child: Divider(),
+        ),
       ),
       backgroundColor: Colors.white,
       bottomNavigationBar: CustomBottonNavigation(index: index, ref: ref),
-      body: const SingleChildScrollView(child: Formulario()),
+      body: SingleChildScrollView(
+        child: Formulario(
+          placaController: placaController,
+          cantidadController: cantidadController,
+          fechaController: fechaController,
+          folioController: folioController,
+          folioPagoController: folioPagoController,
+          cantidadFolioController: cantidadFolioController,
+          insfraccionController: insfraccionController,
+          fechasPagoController: fechasPagoController,
+          foraneasController: foraneasController,
+        ),
+      ),
     );
   }
 }
--- lib/presentaccion/Widget/formulario.dart
+++ lib/presentaccion/Widget/formulario.dart
@@ -6,26 +6,37 @@ import 'package:multas/domain/entiti/list_multas.dart';
 import 'package:multas/presentaccion/provider/read_provider.dart';
 
 class Formulario extends ConsumerStatefulWidget {
+  final TextEditingController placaController;
+  final TextEditingController cantidadController;
+  final TextEditingController fechaController;
+  final TextEditingController folioController;
+  final TextEditingController folioPagoController;
+  final TextEditingController cantidadFolioController;
+  final TextEditingController insfraccionController;
+  final TextEditingController fechasPagoController;
+  final TextEditingController foraneasController;
+
   const Formulario({
-    super.key,
-  });
+    required this.placaController,
+    required this.cantidadController,
+    required this.fechaController,
+    required this.folioController,
+    required this.folioPagoController,
+    required this.cantidadFolioController,
+    required this.insfraccionController,
+    required this.fechasPagoController,
+    required this.foraneasController,
+    Key? key,
+  }) : super(key: key);
 
   @override
   FormularioState createState() => FormularioState();
 }
 
 class FormularioState extends ConsumerState<Formulario> {
-  TextEditingController placaController = TextEditingController();
-  TextEditingController cantidadController = TextEditingController();
-  TextEditingController fechaController = TextEditingController();
-  TextEditingController folioController = TextEditingController();
-  TextEditingController folioPagoController = TextEditingController();
-  TextEditingController cantidadFolioController = TextEditingController();
-  TextEditingController insfraccionController = TextEditingController();
-  TextEditingController fechasPagoController = TextEditingController();
-  TextEditingController foraneasController = TextEditingController();
   final int municipio = 72;
   final String status = 'A';
+
   @override
   Widget build(BuildContext context) {
     final sized = MediaQuery.of(context).size;
@@ -36,35 +47,35 @@ class FormularioState extends ConsumerState<Formulario> {
       child: Wrap(
         children: [
           _Campos(
-            textController: placaController,
+            textController: widget.placaController,
             label: 'Placa',
             icon: Icons.car_crash_outlined,
           ),
           _Campos(
-            textController: cantidadController,
+            textController: widget.cantidadController,
             label: 'Cantidad',
             icon: Icons.currency_exchange,
             numericOnly: true,
           ),
           _Campos(
-            textController: fechaController,
+            textController: widget.fechaController,
             label: 'Fecha',
             icon: Icons.calendar_month_sharp,
             helper: 'Ejemplo: 22/03/2024',
           ),
           _Campos(
-            textController: folioController,
+            textController: widget.folioController,
             label: 'Folio',
             icon: Icons.car_crash_outlined,
             numericOnly: true,
           ),
           _Campos(
-            textController: folioPagoController,
+            textController: widget.folioPagoController,
             label: 'Folio de pago',
             icon: Icons.folder_outlined,
           ),
           _Campos(
-            textController: cantidadFolioController,
+            textController: widget.cantidadFolioController,
             label: 'Cantidad de folio',
             icon: Icons.currency_exchange,
             numericOnly: true,
@@ -84,50 +95,38 @@ class FormularioState extends ConsumerState<Formulario> {
             ),
           ),
           _Campos(
-            textController: fechasPagoController,
+            textController: widget.fechasPagoController,
             label: 'Fecha de Pago',
             icon: Icons.calendar_month_outlined,
             helper: 'Ejemplo: 22/03/2024',
           ),
           _Campos(
-            textController: foraneasController,
+            textController: widget.foraneasController,
             label: 'Foraneas',
           ),
           _ButtonAction(
             onPressed: () {
-              setState(() {
-                _validacion(
-                    context,
-                    municipio,
-                    status,
-                    placaController.text,
-                    cantidadController.text,
-                    fechaController.text,
-                    folioController.text,
-                    folioPagoController.text,
-                    cantidadFolioController.text,
-                    check,
-                    fechasPagoController.text,
-                    foraneasController.text);
-              });
+              _validacion(
+                context,
+                municipio,
+                status,
+                widget.placaController.text,
+                widget.cantidadController.text,
+                widget.fechaController.text,
+                widget.folioController.text,
+                widget.folioPagoController.text,
+                widget.cantidadFolioController.text,
+                check,
+                widget.fechasPagoController.text,
+                widget.foraneasController.text,
+              );
             },
             icon: Icons.add_circle_outlined,
             label: 'Agregar',
           ),
           _ButtonAction(
             onPressed: () {
-              setState(() {
-                placaController.clear();
-                cantidadController.clear();
-                fechaController.clear();
-                folioController.clear();
-                folioPagoController.clear();
-                cantidadFolioController.clear();
-                insfraccionController.clear();
-                fechasPagoController.clear();
-                foraneasController.clear();
-              });
-              ref.read(checkBox.notifier).state = false;
+              _limpiarCampos();
             },
             icon: Icons.delete_forever,
             label: 'Cancelar',
@@ -136,12 +135,87 @@ class FormularioState extends ConsumerState<Formulario> {
       ),
     );
   }
+
+  void _limpiarCampos() {
+    widget.placaController.clear();
+    widget.cantidadController.clear();
+    widget.fechaController.clear();
+    widget.folioController.clear();
+    widget.folioPagoController.clear();
+    widget.cantidadFolioController.clear();
+    widget.insfraccionController.clear();
+    widget.fechasPagoController.clear();
+    widget.foraneasController.clear();
+    ref.read(checkBox.notifier).state = false;
+  }
+
+  void _validacion(
+    BuildContext context,
+    int municipio,
+    String status,
+    String placa,
+    String cantidad,
+    String fecha,
+    String folio,
+    String folioPago,
+    String cantidadFolio,
+    bool check,
+    String fechasPago,
+    String foraneas,
+  ) {
+    if (placa.isEmpty ||
+        cantidad.isEmpty ||
+        fecha.isEmpty ||
+        folio.isEmpty ||
+        folioPago.isEmpty ||
+        cantidadFolio.isEmpty ||
+        fechasPago.isEmpty ||
+        foraneas.isEmpty) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          duration: Duration(milliseconds: 500),
+          backgroundColor: Colors.black12,
+          content: ListTile(
+            title: Text('Algo salió mal'),
+            subtitle: Text('Revisar, existen campos vacíos'),
+          ),
+        ),
+      );
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          duration: Duration(milliseconds: 500),
+          backgroundColor: Colors.black12,
+          content: ListTile(
+            title: Text('Excelente'),
+            subtitle: Text('Todo bien'),
+          ),
+        ),
+      );
+      ListMultas multa = ListMultas(
+        municipio: municipio,
+        status: status,
+        placa: placa,
+        cantidad: double.parse(cantidad),
+        fecha: fecha,
+        folio: int.parse(folio),
+        folioPago: folioPago,
+        cantidadPago: double.parse(cantidad),
+        infraccion: check,
+        fechaPago: fechasPago,
+        foraneas: foraneas,
+      );
+
+      ConnectionMysql().insertQuery(multa);
+    }
+  }
 }
 
 class _ButtonAction extends StatelessWidget {
   final VoidCallback onPressed;
   final IconData? icon;
   final String label;
+
   const _ButtonAction({
     required this.onPressed,
     this.icon,
@@ -167,6 +241,7 @@ class _Campos extends StatelessWidget {
   final String label;
   final bool numericOnly;
   final String? helper;
+
   const _Campos({
     required this.textController,
     required this.label,
@@ -190,9 +265,7 @@ class _Campos extends StatelessWidget {
         width: sized.width <= 800 ? sized.width * 0.7 : sized.width * 0.2,
         child: TextField(
           controller: textController,
-          keyboardType: numericOnly
-              ? TextInputType.number
-              : null, // Solo números si se indica
+          keyboardType: numericOnly ? TextInputType.number : null,
           inputFormatters: numericOnly
               ? [FilteringTextInputFormatter.allow(RegExp(r'[0-9]'))]
               : null,
@@ -206,64 +279,5 @@ class _Campos extends StatelessWidget {
         ),
       ),
     );
-  }
-}
-
-void _validacion(
-    BuildContext context,
-    int municipio,
-    String status,
-    String placa,
-    String cantidad,
-    String fecha,
-    String folio,
-    String folioPago,
-    String cantidadFolio,
-    bool check,
-    String fechasPago,
-    String foraneas) {
-  if (placa.isEmpty ||
-      cantidad.isEmpty ||
-      fecha.isEmpty ||
-      folio.isEmpty ||
-      folioPago.isEmpty ||
-      cantidadFolio.isEmpty ||
-      fechasPago.isEmpty ||
-      foraneas.isEmpty) {
-    ScaffoldMessenger.of(context).showSnackBar(
-      const SnackBar(
-        duration: Duration(milliseconds: 500),
-        backgroundColor: Colors.black12,
-        content: ListTile(
-          title: Text('Algo salio Mal'),
-          subtitle: Text('Revisar, Existen campos vacios'),
-        ),
-      ),
-    );
-  } else {
-    ScaffoldMessenger.of(context).showSnackBar(
-      const SnackBar(
-        duration: Duration(milliseconds: 500),
-        backgroundColor: Colors.black12,
-        content: ListTile(
-          title: Text('Excelente'),
-          subtitle: Text('Todo bien'),
-        ),
-      ),
-    );
-    ListMultas multa = ListMultas(
-        municipio: municipio,
-        status: status,
-        placa: placa,
-        cantidad: double.parse(cantidad),
-        fecha: fecha,
-        folio: int.parse(folio),
-        folioPago: folioPago,
-        cantidadPago: double.parse(cantidad),
-        infraccion: check,
-        fechaPago: fechasPago,
-        foraneas: foraneas);
-
-    ConnectionMysql().insertQuery(multa);
   }
 }
